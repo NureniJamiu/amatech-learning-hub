9fd01633638ea51ab469e4f5162ca47a499a88c1
NureniJamiu <nurenijamiu951@gmail.com>
2025-05-29 15:50:05 +0100

    add tanstack query provide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/app/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { QueryProvider } from "@/providers/query-provider";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 // import { ThemeProvider } from "@/components/theme-provider"
 // import { Toaster } from "@/components/ui/toaster"
@@ -11,25 +12,29 @@ import { QueryProvider } from "@/providers/query-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Amatech Lasu - Student Learning Hub",
-  description: "Access course materials and resources",
+    title: "Amatech Lasu - Student Learning Hub",
+    description: "Access course materials and resources",
 };
 
+const queryClient = new QueryClient();
+
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <QueryProvider>
-          {/* <ThemeProvider attribute="class" defaultTheme="light" enableSystem> */}
-          {children}
-          {/* <Toaster /> */}
-          {/* </ThemeProvider> */}
-        </QueryProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <QueryClientProvider client={queryClient}>
+                <body className={inter.className}>
+                    <QueryProvider>
+                        {/* <ThemeProvider attribute="class" defaultTheme="light" enableSystem> */}
+                        {children}
+                        {/* <Toaster /> */}
+                        {/* </ThemeProvider> */}
+                    </QueryProvider>
+                </body>
+            </QueryClientProvider>
+        </html>
+    );
 }
