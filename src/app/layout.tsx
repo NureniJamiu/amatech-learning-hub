9444c514a1723b108651e37bc1a0e5a4ac363c6c
NureniJamiu <nurenijamiu951@gmail.com>
2025-08-07 08:28:16 +0100

    feat: update project configuration and layout
    - Update app layout with improved structure
    - Update package.json with new dependencies
    - Update pnpm-lock.yaml with latest package versions
    - Update Prisma schema with enhanced database models
    - Remove deprecated course API file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "@/app/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { QueryProvider } from "@/providers/query-provider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // import { ThemeProvider } from "@/components/theme-provider"
 // import { Toaster } from "@/components/ui/toaster"
@@ -26,6 +28,17 @@ export default function RootLayout({
                 <QueryProvider>
                     {/* <ThemeProvider attribute="class" defaultTheme="light" enableSystem> */}
                     {children}
+                    <ToastContainer
+                        position="top-right"
+                        autoClose={4000}
+                        hideProgressBar={false}
+                        newestOnTop
+                        closeOnClick
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="colored"
+                    />
                     {/* <Toaster /> */}
                     {/* </ThemeProvider> */}
                 </QueryProvider>
